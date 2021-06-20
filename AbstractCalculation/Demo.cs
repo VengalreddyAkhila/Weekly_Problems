bd53c00c7204aa95861f3bade703192adbb18025
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-20 10:59:11 +0530

    [akhila]Added:Abstract and interface problrm

---
+++ AbstractCalculation/Demo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceCalculation
+{
+    /// <summary>
+    /// problem caluclator calling abstractclass and interface  in new class
+    /// </summary>
+    
+    //interface with methods
+    interface IDemo
+    {
+        public void Add();
+        public void Substract();
+        const double pi = 3.146;
+    }  
+    /// <summary>
+    /// abstract class with methods
+    /// </summary>
+    abstract class Calculator
+    {
+        public  void Multiplication()
+        {
+            int firstnum = 2;
+            int secondnum = 3;
+            int mul = firstnum * secondnum;
+            Console.WriteLine(mul);
+        }
+
+        public void Division()
+        {
+            int firstnum = 10;
+            int secondnum = 5;
+            int div = firstnum / secondnum;
+            Console.WriteLine(div);
+        }
+        public abstract void Increment(int firstnum);
+        public abstract  void Mod(int firstNum);
+
+    }    
+    class Circular :Calculator , IDemo 
+    {
+        /// <summary>
+        /// calculate area of a circle
+        /// area = PI* Radius * Radius
+       /// </summary>        
+        public void Area()
+        {
+            int radius = 3;
+            double area = IDemo.pi * Math.Pow(radius,2);
+            Console.WriteLine(area);
+        }
+        /// <summary>
+        /// Add method inherited from Interface
+        /// </summary>
+        public void Add()
+        {
+            int firstnum = 10;
+            int secondnum = 20;
+            int Add = firstnum + secondnum;
+            Console.WriteLine(Add);
+        }
+        /// <summary>
+        /// substract method inherited from interface
+        /// </summary>
+        public void Substract()
+        {
+            int firstnum = 20;
+            int secondnum = 10;
+            int sub = firstnum - secondnum;
+            Console.WriteLine(sub);
+        }      
+        /// <summary>
+        /// increment method inherited from abstract class
+        /// </summary>
+        /// <param name="firstnum"></param>
+        public override void Increment(int firstnum)
+        {
+            int value1 = ++firstnum;
+            Console.WriteLine("" + value1);
+        }
+        /// <summary>
+        /// mod method inherited from abstract class
+        /// </summary>
+        /// <param name="firstnum"></param>
+        public override void Mod(int firstnum)
+        {            
+            int value = firstnum % 2;
+            Console.WriteLine(value);
+        }
+    }
+        
+}
---
+++ AbstractCalculation/Program.cs
@@ -0,0 +1,47 @@
+﻿using InterfaceCalculation;
+using System;
+
+namespace AbstractCalculation
+{
+    class Program
+    {
+        /// <summary>
+        /// problem calculator
+        /// interface
+        /// methods
+        /// 1.add
+        /// 2.substract
+        /// const is const double pi = 3.146
+        /// 
+        /// Abstract class
+        /// 1.multiplication//normal method
+        /// 2.division    //normal method
+        /// 3.abstract mod  //abstract method
+        /// 4.abstract increment //abstract method
+        /// 
+        /// New class
+        /// that inherit both abstract and interface and implement methods,
+        /// 1.circular area (int radius)//normal method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {         
+
+            Console.WriteLine("Welcome to problem caluclator");
+             //output for interface
+             IDemo  demo = new Circular();
+             demo.Add();
+             demo.Substract();
+
+             //output for abstract class
+             Calculator cal = new Circular();
+             cal.Division();
+             cal.Multiplication();
+             cal.Mod(100);
+             cal.Increment(5);
+             //output for circle class
+             Circular circle = new Circular();
+             circle.Area();
+        }
+    }
+}
